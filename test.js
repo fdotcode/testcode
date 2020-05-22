e921ccf03a69d19f0179ea16e67972b13208caf6
fdotcode <63472579+fdotcode@users.noreply.github.com>
2020-05-22 14:01:48 -0400

    test code roll

---
+++ test.js
@@ -0,0 +1,30 @@
+var Answer= prompt('What is your name:');
+
+
+var AnswerLength = Answer.length;
+
+console.log(AnswerLength);
+
+alert("Your words are " + AnswerLength + " Chars long");
+
+alert("You have typed " +AnswerLength + "Chars, You have  "+ (140-AnswerLength) + "Chars remaining" );
+
+
+
+//console.log(Answer);
+
+//console.log(jojo);
+
+//Age=prompt("What year where you born?");
+
+//year = 2020;
+
+//repy=year-Age;
+
+//console.log( "You are about " + repy + " years old");
+
+//alert(Age);
+
+var joe="sadwords"
+
+console.log(joe)
