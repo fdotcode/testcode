2a76aa0dc0ba91acfd431040c92e772fbc78050e
fdotcode <63472579+fdotcode@users.noreply.github.com>
2020-05-22 17:35:34 -0400

    Add files via upload

--- test.js
+++ test.js
@@ -36,6 +36,16 @@ Qanswer = prompt("is House Dubois thee house of all ages ? y for Yes : ")
 if (Qanswer ==="y" ) {
   alert('HELLOOOO to life');
 }
+
+else if (Qanswer==="Y") {
+  alert('HELLOOOO to life');
+
+}
+
+else if (Qanswer==="YES") {
+  alert('HELLOOOO to life');
+
+}
 else{
   alert("off with your head");
 }
